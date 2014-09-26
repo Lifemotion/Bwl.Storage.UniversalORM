280d22eb3554d082c86cf669d64d8d398e91e3c0
pushka <pushka@PAVELCHE>
2014-09-26 11:55:20 +0400

    В Firebird исправлена проверка БД на существование.
    Теперь поддерживается подключение по алиасам(они настраиваются в файле aliases.conf в папке сервера).
    
    
    Former-commit-id: b144cd69076376dd6f3e7910f6d9a97abe12749d

--- Bwl.Storage.UniversalORM.Firebird/FBUtils.vb
+++ Bwl.Storage.UniversalORM.Firebird/FBUtils.vb
@@ -48,34 +48,14 @@ Public Class FbUtils
 	End Function
 
 	Public Shared Sub CreateDB(connStringBld As FbConnectionStringBuilder, dbName As String)
-		If Not IO.File.Exists(connStringBld.Database) Then
-			Try
-				FbConnection.CreateDatabase(connStringBld.ToString())
-			Catch ex As Exception
-				Throw New Exception("Ошибка. " + ex.Message)
-			End Try
-		End If
 		If (Not FbUtils.CheckConnection(connStringBld.ConnectionString)) Then
 			Threading.Thread.Sleep(2000)
 			If (Not FbUtils.CheckConnection(connStringBld.ConnectionString)) Then
-				Dim conStrBld2 = New FbConnectionStringBuilder
-				conStrBld2.ConnectionTimeout = connStringBld.ConnectionTimeout
-				conStrBld2.ServerType = connStringBld.ServerType
-				conStrBld2.UserID = connStringBld.UserID
-				conStrBld2.Password = connStringBld.Password
-				conStrBld2.Dialect = connStringBld.Dialect
-				conStrBld2.Database = connStringBld.Database
-				conStrBld2.ClientLibrary = connStringBld.ClientLibrary
-				Dim sql = String.Format("CREATE DATABASE [{0}]", dbName)
-				FbUtils.ExecSQL(conStrBld2.ConnectionString, sql)
-				Threading.Thread.Sleep(2000)
+				FbConnection.CreateDatabase(connStringBld.ToString())
 			End If
 		End If
 	End Sub
 
-	Public Shared Sub CreateTable(connStringBld As FbConnectionStringBuilder, tableName As String)
-
-	End Sub
 
 	Public Shared Function TableExists(connString As String, tableName As String) As Boolean
 		Dim res = False
--- Tests/Bwl.Storage.UniversalORM.LocalStorage.CommonUnitTests/LocalSettings_Firebird.vb
+++ Tests/Bwl.Storage.UniversalORM.LocalStorage.CommonUnitTests/LocalSettings_Firebird.vb
@@ -4,8 +4,8 @@ Imports FirebirdSql.Data.FirebirdClient
 
 Public Class LocalSettings_Firebird
 	Private ReadOnly _settings As SettingsStorage
-	Private ReadOnly _connStrBld_embed As FbConnectionStringBuilder
-	Private ReadOnly _connStrBld_service As FbConnectionStringBuilder
+	Private _connStrBld_embed As FbConnectionStringBuilder
+	Private _connStrBld_service As FbConnectionStringBuilder
 
 	Public Sub New(settings As SettingsStorage)
 		_settings = settings
@@ -15,21 +15,23 @@ Public Class LocalSettings_Firebird
 		'  их надо изменять в конфиг файле
 		'!!!!!!!!!!!!!!!!!!!!!
 
-		Dim dbStorage = _settings.CreateChildStorage("DB_FB")
+		CreateFbEmbedBld()
+		CreateFbServiceBld()
+	End Sub
 
-		Dim userSetting = dbStorage.CreateStringSetting("DBUserSetting", "sysdba")
-		Dim passSetting = dbStorage.CreateStringSetting("DBPassSetting", "masterkey")
-		Dim dialectSetting = dbStorage.CreateIntegerSetting("DialectSetting", 3)
+	Private Sub CreateFbEmbedBld()
+		Dim dbStorage_embed = _settings.CreateChildStorage("DB_FB_embed")
+
+		Dim userSetting = dbStorage_embed.CreateStringSetting("DBUserSetting", "sysdba")
+		Dim passSetting = dbStorage_embed.CreateStringSetting("DBPassSetting", "masterkey")
+		Dim dialectSetting = dbStorage_embed.CreateIntegerSetting("DialectSetting", 3)
 
 		Dim DBPathDef_embed = IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\data\TestDB_EMBEDDED.fdb")
-		Dim databaseSetting_embed = dbStorage.CreateStringSetting("DatabaseSetting_Embed", DBPathDef_embed)
+		Dim databaseSetting_embed = dbStorage_embed.CreateStringSetting("DatabaseSetting_Embed", DBPathDef_embed)
 
-		Dim DBPathDef_service = IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\data\TestDB_service.fdb")
-		Dim databaseSetting_service = dbStorage.CreateStringSetting("DatabaseSetting_Service", DBPathDef_service)
-
-		Dim dbTimeout = dbStorage.CreateIntegerSetting("dbTimeout", 1)
+		Dim dbTimeout = dbStorage_embed.CreateIntegerSetting("dbTimeout", 1)
 		Dim clientDllPathDef = IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "fbe32\fbembed.dll")
-		Dim clientDllPathSetting = dbStorage.CreateStringSetting("clientDllPathSetting", clientDllPathDef)
+		Dim clientDllPathSetting = dbStorage_embed.CreateStringSetting("clientDllPathSetting", clientDllPathDef)
 
 		_connStrBld_embed = New FbConnectionStringBuilder()
 		_connStrBld_embed.Database = databaseSetting_embed.Value
@@ -39,6 +41,21 @@ Public Class LocalSettings_Firebird
 		_connStrBld_embed.Dialect = dialectSetting.Value
 		_connStrBld_embed.ConnectionTimeout = dbTimeout.Value
 		_connStrBld_embed.ClientLibrary = clientDllPathSetting.Value
+	End Sub
+
+	Private Sub CreateFbServiceBld()
+		Dim dbStorage_service = _settings.CreateChildStorage("DB_FB_service")
+
+		Dim userSetting = dbStorage_service.CreateStringSetting("DBUserSetting", "sysdba")
+		Dim passSetting = dbStorage_service.CreateStringSetting("DBPassSetting", "masterkey")
+		Dim dialectSetting = dbStorage_service.CreateIntegerSetting("DialectSetting", 3)
+
+		Dim dataSourceSetting = dbStorage_service.CreateStringSetting("DataSourceSetting", "localhost")
+
+		Dim DBPathDef_service = IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\data\TestDB_service.fdb")
+		Dim databaseSetting_service = dbStorage_service.CreateStringSetting("DatabaseSetting_Service", DBPathDef_service)
+
+		Dim dbTimeout = dbStorage_service.CreateIntegerSetting("dbTimeout", 1)
 
 		_connStrBld_service = New FbConnectionStringBuilder()
 		_connStrBld_service.Database = databaseSetting_service.Value
@@ -46,7 +63,7 @@ Public Class LocalSettings_Firebird
 		_connStrBld_service.Password = passSetting.Value
 		_connStrBld_service.Dialect = dialectSetting.Value
 		_connStrBld_service.ConnectionTimeout = dbTimeout.Value
-		'_connStrBld_service.ClientLibrary = clientDllPathSetting.Value
+		_connStrBld_service.DataSource = dataSourceSetting.Value
 	End Sub
 
 	Public ReadOnly Property ConnectionStringBuilder_Service As FbConnectionStringBuilder

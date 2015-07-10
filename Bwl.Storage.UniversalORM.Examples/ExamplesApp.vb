44f731bb3636521fc724854ba480d1ff6bc8967f
Igor Koshelev <igor@lifemotion.ru>
2015-07-10 16:48:24 +0300

    Улучшил пример немного.

--- Bwl.Storage.UniversalORM.Examples/ExamplesApp.vb
+++ Bwl.Storage.UniversalORM.Examples/ExamplesApp.vb
@@ -39,7 +39,7 @@ Public Class ExamplesApp
         'создание хранилища для типа данных TestDataClass
         Dim fbStorageForTestData = fbStorageManager.CreateStorage(Of TestDataClass)("TestDataClass")
         'создание двух объектов с разными данными
-        Dim obj1 As New TestDataClass With {.Cat = "cat1", .Dog = 1, .Timestamp = Now, .NestedData = New TestDataNestedClass With {.First = "first1"}}
+        Dim obj1 As New TestDataClass With {.Cat = "cat1", .Dog = 1, .Timestamp = Now, .NestedData = New TestDataNestedClass With {.First = "first1", .Second = 1}}
         Dim obj2 As New TestDataClass With {.Cat = "cat2", .Dog = 2, .Timestamp = Now.AddHours(-1), .NestedData = New TestDataNestedClass With {.First = "first2"}}
         'очистка хранилища
         fbStorageForTestData.RemoveAllObjects()
@@ -48,11 +48,13 @@ Public Class ExamplesApp
         fbStorageForTestData.AddObj(obj2)
         'поиск идентификаторов объектов без ограничения
         Dim objSearchResults1a = fbStorageForTestData.FindObj(New SearchParams)
-        'поиск идентификаторов объектов, где поле Cat равно значению cat1
+        'поиск идентификаторов объектов, где поле Cat равно cat1
         'поиск возможен только по полям Indexing
         Dim objSearchResults1b = fbStorageForTestData.FindObj(New SearchParams({New FindCriteria("Cat", FindCondition.eqaul, "cat1")}))
-        'поиск идентификаторов объектов, где поле Cat содержит значению cat1
+        'поиск идентификаторов объектов, где поле Cat содержит cat
         Dim objSearchResults1c = fbStorageForTestData.FindObj(New SearchParams({New FindCriteria("Cat", FindCondition.likeEqaul, "cat")}))
+        'поиск идентификаторов объектов, где поле Second вложенного объекта NestedData равно 1
+        Dim objSearchResults1d = fbStorageForTestData.FindObj(New SearchParams({New FindCriteria("NestedData.Second", FindCondition.eqaul, 1)}))
         'извлечение объекта из хранилища по идентификатору
         Dim obj1restored As TestDataClass = fbStorageForTestData.GetObj(objSearchResults1a(0))
         _logger.AddMessage("FirebirdStorageExample - ok")

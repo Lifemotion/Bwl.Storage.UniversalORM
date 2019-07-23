a0b108f9dc839ac9e7175b5a9f0cf60257bddb71
draf <artem.drobanov@gmail.com>
2019-07-23 17:03:10 +0300

    Корректировка игнора AssemblyInfo после маркировки.

---
+++ Bwl.Storage.UniversalORM.Firebird/My Project/AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором 
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Bwl.Storage.UniversalORM.Firebird")> 
+<Assembly: AssemblyDescription("18fb781 Убрана очистка пулов")>
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Bwl.Storage.UniversalORM.Firebird")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("27494076-5ce9-4601-99fd-2c7dd4506d86")> 
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'      Номер построения
+'      Редакция
+'
+' Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyInformationalVersion("{c:ymd.} {c:hms:} {chash:6}")>
---
+++ Bwl.Storage.UniversalORM/My Project/AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором 
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Bwl.Storage.UniversalORM")> 
+<Assembly: AssemblyDescription("18fb781 Убрана очистка пулов")>
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Bwl.Storage.UniversalORM")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("f0eb7954-8e9d-4e55-9e78-3a45a7d85ad0")> 
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyInformationalVersion("{c:ymd.} {c:hms:} {chash:6}")>
